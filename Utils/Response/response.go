f8a93d377c09cd8360fbe14fe617e436437e6e17
wucongxing <815046773@qq.com>
2022-06-27 11:10:17 +0800

    新增工具类-返回数据格式定义

---
+++ Utils/Response/response.go
@@ -0,0 +1,58 @@
+package Response
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	ERROR   = 1
+	SUCCESS = 0
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func Result(code int, data interface{}, msg string, c *gin.Context) {
+	if data == nil {
+
+		data = gin.H{}
+
+	}
+	c.JSON(http.StatusOK, Response{
+		code,
+		data,
+		msg,
+	})
+}
+
+func Ok(c *gin.Context) {
+	Result(SUCCESS, map[string]interface{}{}, "成功", c)
+}
+
+func OkWithMessage(message string, c *gin.Context) {
+	Result(SUCCESS, map[string]interface{}{}, message, c)
+}
+
+func OkWithData(data interface{}, c *gin.Context) {
+	Result(SUCCESS, data, "成功", c)
+}
+
+func OkWithDetailed(data interface{}, message string, c *gin.Context) {
+	Result(SUCCESS, data, message, c)
+}
+
+func Fail(c *gin.Context) {
+	Result(ERROR, map[string]interface{}{}, "成功", c)
+}
+
+func FailWithMessage(message string, c *gin.Context) {
+	Result(ERROR, map[string]interface{}{}, message, c)
+}
+
+func FailWithDetailed(data interface{}, message string, c *gin.Context) {
+	Result(ERROR, data, message, c)
+}
